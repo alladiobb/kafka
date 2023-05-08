7f7746b305e37b6bda4e076212ba9c2597e40c08
Alladio <alladio_bonesso@hotmail.com>
2023-05-07 21:05:55 -0300

    atualizacao

---
+++ config.sh
@@ -0,0 +1,9 @@
+#instalando o kafka
+#denro do diretório do arqquivo docker-compose.yaml:
+docker-compose up -d
+
+#pegue o nome em que o kafka está rodando
+docker-compose ps
+
+#Execute o comando para entrar no TERMINAL do kafka
+docker exec -it kafka-kafaka-1 bash
