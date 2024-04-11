b11ef977fce18af5b31dc20c7cd37f3fc42d194d
Alladio Bonesso <13860253+alladiobb@users.noreply.github.com>
2024-04-11 14:40:15 -0300

    Refact: Docker Atualização

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #instalando o kafka
 #denro do diretório do arqquivo docker-compose.yaml:
-docker-compose up -d
+docker compose up -d
 
 #pegue o nome em que o kafka está rodando
-docker-compose ps
+docker compose ps
 
 #Execute o comando para entrar no TERMINAL do kafka
-docker exec -it kafka-kafaka-1 bash
+docker exec -it kafkadocker-kafka-1 bash
 
 #Comando que serão usados:
 #bootstrap-server
