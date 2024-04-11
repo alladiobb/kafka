4a017217fc6e9604db724f3f8a99045aa6b30992
Alladio Bonesso <13860253+alladiobb@users.noreply.github.com>
2024-04-11 14:59:21 -0300

    Add: Consumer groups

--- config.sh
+++ config.sh
@@ -8,6 +8,9 @@ docker compose ps
 #Execute o comando para entrar no TERMINAL do kafka
 docker exec -it kafkadocker-kafka-1 bash
 
+#entendendo o kafka-topics
+kafka-topics
+
 #Comando que serão usados:
 #bootstrap-server
 #topic
@@ -18,8 +21,17 @@ docker exec -it kafkadocker-kafka-1 bash
 #criado o tópico
 kafka-topics --create --topic=teste --bootstrap-server=localhost:9092 --partitions=3
 
+#Listando o tópico criado e outros que existem
+kafka-topics --list --bootstrap-server=localhost:9092
+
 #descrevendo o tópico
 kafka-topics --topic=teste --bootstrap-server=localhost:9092 --describe
+
+# Topic: teste    PartitionCount: 3       ReplicationFactor: 1    Configs:
+#         Topic: teste    Partition: 0    Leader: 1       Replicas: 1     Isr: 1
+#         Topic: teste    Partition: 1    Leader: 1       Replicas: 1     Isr: 1
+#         Topic: teste    Partition: 2    Leader: 1       Replicas: 1     Isr: 1
+
 
 #criando o consumidor
 kafka-console-consumer --bootstrap-server=localhost:9092 --topic=teste
@@ -29,3 +41,6 @@ kafka-console-producer --bootstrap-server=localhost:9092 --topic=teste
 
 #criando o consumidor e lendo desde o começo as mensagens
 kafka-console-consumer --bootstrap-server=localhost:9092 --topic=teste --from-beginning
+
+#Consumer Groups
+kafka-console-consumer --bootstrap-server=localhost:9092 --topic=teste group=x
