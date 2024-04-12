68abd31af746f8ad7c365260fb21cc996c207db5
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-04-12 19:38:35 -0300

    Feat: Producer and Consumer update

---
+++ KafkaAndGo/cmd/consumer/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "kafka-kafka-1:9092",
+		"client.id":         "goapp-consumer",
+		"grouip.id":         "goapp-group",
+		"auto.offset.reset": "earliest",
+	}
+
+	c, err := kafka.NewConsumer(configMap)
+
+	if err != nil {
+		fmt.Println("error Consumer", err.Error())
+	}
+	topics := []string{"teste"}
+	c.SubscribeTopics(topics, nil)
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			fmt.Println(string(msg.Value), msg.TopicPartition)
+		}
+	}
+
+}
---
+++ KafkaAndGo/cmd/producer/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	//chan é um canal que o
+	deliveryChannel := make(chan kafka.Event)
+	producer := NewKafkaProducer()
+
+	// Essa linha da "KEY" Não deixa mandar para outras partições. No caso não vai ter problema da ordem de envio
+	key := []byte("testees")
+
+	Publish("mensagem", "teste", producer, key, deliveryChannel)
+	// Joga em uma thread de forma assincrona
+	go DeliveryReport(deliveryChannel)
+	// e := <-deliveryChannel
+	// msg_return := e.(*kafka.Message)
+
+	// if msg_return.TopicPartition.Error != nil {
+	// 	fmt.Println("Error ao enviar msg!")
+	// } else {
+	// 	fmt.Println("Mensagem Enviada", msg_return.TopicPartition)
+	// }
+
+	producer.Flush(1000)
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers":   "kafka-kafka-1:9092",
+		"delivery.timeout.ms": "0",
+		"acks":                "all",
+		"enable.idempotence":  "true",
+	}
+
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	return p
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChannel chan kafka.Event) error {
+	message := &kafka.Message{
+		Value:          []byte(msg),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+	err := producer.Produce(message, deliveryChannel)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeliveryReport(deliveryChannel chan kafka.Event) {
+	for e := range deliveryChannel {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Println("Error ao enviar msg!")
+			} else {
+				fmt.Println("Mensagem Enviada", ev.TopicPartition)
+			}
+		}
+	}
+}
